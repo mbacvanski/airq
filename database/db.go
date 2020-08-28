5342e78c48c246206b358c9f830d99c7a3cd2c11
marc <marc.bacvanski@gmail.com>
2020-08-28 12:22:20 -0700

    Connection to Mongo

--- database/db.go
+++ database/db.go
@@ -1,22 +1,79 @@
 package database
 
 import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
 	"time"
 )
 
-type DB struct{
-	client mongo.Client
+const DB_NAME string = "airquality"
+const SENSOR_DATA_COLL string = "sensordata"
+
+type DB struct {
+	client               *mongo.Client
+	sensorDataCollection *mongo.Collection
 }
 
-// TODO
-func NewDB() *DB {
+func NewDB() (*DB, error) {
+	// Create the client
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Println("Error creating mongodb client " + err.Error())
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Connect to the database
+	if err := client.Connect(ctx); err != nil {
+		log.Println("Error connecting to mongodb database " + err.Error())
+		return nil, err
+	}
+
+	// Check to make sure the server was connected to
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		log.Println("Error verifying connection to mongodb " + err.Error())
+		return nil, err
+	}
+
+	newDb := DB{
+		client:               client,
+		sensorDataCollection: client.Database(DB_NAME).Collection(SENSOR_DATA_COLL),
+	}
+
+	return &newDb, nil
 }
 
-// TODO
 func (db *DB) WriteDataEntry(timestamp time.Time,
 	sensorName, sensorId string, datapoints map[string]float64) error {
 
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	data := bson.M{
+		"timestamp":  timestamp,
+		"sensorname": sensorName,
+		"sensorid":   sensorId,
+	}
+
+	for key, val := range datapoints {
+		data[key] = val
+	}
+
+	_, err := db.sensorDataCollection.InsertOne(ctx, data)
+	if err != nil {
+		log.Println("Error inserting data entry " + err.Error())
+		return err
+	}
+	return nil
+}
+
+func (db *DB) Close() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_ = db.client.Disconnect(ctx)
 }
--- server.go
+++ server.go
@@ -3,73 +3,101 @@ package main
 import (
 	"airquality/database"
 	"fmt"
-	"github.com/araddon/dateparse"
+	"github.com/btubbs/datetime"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"time"
 )
 
-func dataHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
-		return
-	}
-
-	if err := r.ParseForm(); err != nil {
-		http.Error(w, "Could not parse form stringData", http.StatusBadRequest)
-		return
-	}
-
-	stringData := map[string]string{}
-	stringData["timestampStr"] = r.FormValue("timestamp")            // timestamp of collected reading
-	stringData["sensorName"] = r.FormValue("sensorname")             // name of sensor
-	stringData["sensorId"] = r.FormValue("sensorid")                 // id of sensor
-	stringData["particles003dlStr"] = r.FormValue("particles_003dl") // 00.3 µm per 0.1 liters
-	stringData["particles005dlStr"] = r.FormValue("particles_005dl") // 00.5 µm per 0.1 liters
-	stringData["particles010dlStr"] = r.FormValue("particles_010dl") // 01.0 µm per 0.1 liters
-	stringData["particles025dlStr"] = r.FormValue("particles_025dl") // 02.5 µm per 0.1 liters
-	stringData["particles050dlStr"] = r.FormValue("particles_050dl") // 05.0 µm per 0.1 liters
-	stringData["particles100dlStr"] = r.FormValue("particles_100dl") // 10.0 µm per 0.1 liters
-	stringData["stdPm010"] = r.FormValue("stdPm010")                 // Standardized readings
-	stringData["stdPm025"] = r.FormValue("stdPm025")
-	stringData["stdPm100"] = r.FormValue("stdPm100")
-	stringData["envPm010"] = r.FormValue("envPm010") // Environmental readings
-	stringData["envPm025"] = r.FormValue("envPm025")
-	stringData["envPm100"] = r.FormValue("envPm100")
-
-	for key, val := range stringData {
-		if val == "" {
-			http.Error(w, "Missing one or more values, including "+key, http.StatusBadRequest)
+// Handles incoming data from sensors, writes it into the database.
+// Will fail if any value is missing, corrupted, or not parseable.
+func dataHandler(db *database.DB) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
+			return
 		}
-	}
 
-	timestamp, err := dateparse.ParseAny(stringData["timestampStr"])
-	if err != nil {
-		http.Error(w, "Bad date "+stringData["timestampStr"], http.StatusBadRequest)
-	}
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "Could not parse form stringData", http.StatusBadRequest)
+			return
+		}
 
-	// These are the keys of the data that should be kept as strings.
-	stringKeys := map[string]bool{"timestampStr": true, "sensorName": true, "sensorId": true}
-	// These are the data items that should be floats.
-	floatData := map[string]float64{}
-	for key, val := range stringData {
-		if _, ok := stringKeys[key]; !ok {
-			// Cast it into a float
-			num, err := strconv.ParseFloat(val, 64)
-			if err == nil {
-				floatData[key] = num
-			} else {
-				http.Error(w, "Bad value "+val+" for key "+key, http.StatusBadRequest)
+		stringData := map[string]string{}
+		stringData["timestampStr"] = r.FormValue("timestamp")         // timestamp of collected reading
+		stringData["sensorName"] = r.FormValue("sensorname")          // name of sensor
+		stringData["sensorId"] = r.FormValue("sensorid")              // id of sensor
+		stringData["particles003dl"] = r.FormValue("particles_003dl") // 00.3 µm per 0.1 liters
+		stringData["particles005dl"] = r.FormValue("particles_005dl") // 00.5 µm per 0.1 liters
+		stringData["particles010dl"] = r.FormValue("particles_010dl") // 01.0 µm per 0.1 liters
+		stringData["particles025dl"] = r.FormValue("particles_025dl") // 02.5 µm per 0.1 liters
+		stringData["particles050dl"] = r.FormValue("particles_050dl") // 05.0 µm per 0.1 liters
+		stringData["particles100dl"] = r.FormValue("particles_100dl") // 10.0 µm per 0.1 liters
+		stringData["stdPm010"] = r.FormValue("stdPm010")              // Standardized readings
+		stringData["stdPm025"] = r.FormValue("stdPm025")
+		stringData["stdPm100"] = r.FormValue("stdPm100")
+		stringData["envPm010"] = r.FormValue("envPm010") // Environmental readings
+		stringData["envPm025"] = r.FormValue("envPm025")
+		stringData["envPm100"] = r.FormValue("envPm100")
+
+		fmt.Printf("Received form data %s\n", stringData)
+
+		for key, val := range stringData {
+			if val == "" {
+				http.Error(w, "Missing one or more values, including "+key, http.StatusBadRequest)
+				return
 			}
 		}
-	}
 
-	// TODO
-	newDb := database.NewDB()
-	newDb.WriteDataEntry()
+		timestamp, dateErr := datetime.Parse(stringData["timestampStr"], time.UTC)
+		if dateErr != nil {
+			http.Error(w, "Bad date "+stringData["timestampStr"], http.StatusBadRequest)
+			return
+		}
 
-	fmt.Printf("Received stringData %s\n")
+		// These are the keys of the data that should be kept as strings.
+		stringKeys := map[string]bool{"timestampStr": true, "sensorName": true, "sensorId": true}
+		// These are the data items that should be floats.
+		floatData := map[string]float64{}
+		for key, val := range stringData {
+			if _, ok := stringKeys[key]; !ok {
+				// Cast it into a float
+				num, floatCastErr := strconv.ParseFloat(val, 64)
+				if floatCastErr == nil {
+					floatData[key] = num
+				} else {
+					http.Error(w, "Bad value "+val+" for key "+key, http.StatusBadRequest)
+					return
+				}
+			}
+		}
+
+		if err := db.WriteDataEntry(timestamp, stringData["sensorName"], stringData["sensorId"], floatData); err != nil {
+			http.Error(w, "Could not write data", http.StatusInternalServerError)
+			log.Println("Failed writing data to database: " + err.Error())
+			return
+		}
+	})
 }
 
 func main() {
-	http.Handle("/data", dataHandler)
+	db, err := database.NewDB()
+	if err != nil {
+		panic("Could not connect to database: " + err.Error())
+	}
+
+	http.Handle("/data", dataHandler(db))
+
+	port := os.Getenv("port")
+	if port == "" {
+		log.Fatal("No port specified")
+		return
+	}
+
+	fmt.Printf("Starting server on port %s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
